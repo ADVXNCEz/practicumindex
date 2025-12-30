705273894f40d4c5db3c7ec3c369f64f20866f61
Peeraphan Phuripattaraphan <phuripattaraphan7@gmail.com>
2025-12-30 19:20:22 +0700

    complete_part3

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from flask import render_template
-from datetime import datetime
+from flask import render_template, redirect, url_for
 from flask import request
+
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -34,13 +35,6 @@ def show_news_item(id):
                            title=news_item['title'],
                            body=news_item['body'])
 
-@app.route('/news/create/', methods=['POST'])
-def create_news_item():
-    item = new_news_item(request.form['title'],
-                         request.form['body'])
-    news_items[item['id']] = item
-    return ''
-
 def new_news_item(title, body):
     new_id = max(news_items.keys()) + 1
     return {
@@ -48,3 +42,10 @@ def new_news_item(title, body):
         'title': title,
         'body': body
     }
+
+@app.route('/news/create/', methods=['POST'])
+def create_news_item():
+    item = new_news_item(request.form['title'],
+                         request.form['body'])
+    news_items[item['id']] = item
+    return redirect(url_for('index'))
